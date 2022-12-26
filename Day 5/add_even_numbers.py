3b951f8c23fafad5b2adcf47e8960b7b6a0799e1
Samarth Upadhya <84448650+SamSince02@users.noreply.github.com>
2022-12-26 06:10:10 +0530

    Add files via upload

---
+++ Day 5/add_even_numbers.py
@@ -0,0 +1,5 @@
+
+esum =0
+for i in range(2,101,2):
+    esum += i
+print(esum)
---
+++ Day 5/high_score.py
@@ -0,0 +1,14 @@
+
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+maxele=0
+for i in student_scores:
+    if i > maxele:
+        maxele = i
+print(f"The highest score in the class is: {maxele}")
+
+
+
